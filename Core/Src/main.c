eaf402ea776e35c07f7edbdbb25cb2395f52eec0
cma <cmahlburg@posteo.de>
2023-03-19 16:38:15 +0100

    fix 0xFF bug on uart startup
    - use internal pull-up to avoid creating unintentional startbit on uart line
    
    Signed-off-by: cma <cmahlburg@posteo.de>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -103,8 +103,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  CLI_print("\r\n"); /* TODO always sends 0xFF on first transmit */
-  CLI_print("Startup!\r\n");
+  CLI_print("\r\nStartup!\r\n");
 
   /* Start the tasks and timer running. */
   vTaskStartScheduler();
@@ -258,7 +257,7 @@ static void MX_USART1_UART_Init(void)
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
   GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
